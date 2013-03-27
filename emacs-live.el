e31f375956ff534f16b5e50bc57b2952d6a24530
hp-laptop <sorabji52@gmail.com>
2013-03-27 12:48:04 -0700

    config file added

---
+++ emacs-live.el
@@ -0,0 +1,4 @@
+(live-add-packs '(~/config/emacs-live-packs/php-pack
+                  ~/config/emacs-live-packs/function-pack
+                  ~/config/emacs-live-packs/emms-pack
+                  ~/config/emacs-live-packs/go-pack))
